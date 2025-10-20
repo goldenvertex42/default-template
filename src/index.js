e88e6e06c002c291c2e884f47f1792cd3f7d0a16
goldenvertex42 <fabled93@gmail.com>
2025-10-19 21:31:40 -0500

    Create barebones infrastructure for webpack project template

--- src/index.js
+++ src/index.js
@@ -1,23 +1,3 @@
 import "./styles.css";
-import { DropdownMenu } from "./dropdown"
-import { setupEventListeners } from "./events";
 
-export const initialize = () => {
-    document.addEventListener('DOMContentLoaded', () => {
-        const dropdownElements = document.querySelectorAll('.dropdown');
-        const dropdownInstances = [];
-
-        dropdownElements.forEach(element => {
-            const dropdownInstance = new DropdownMenu(element);
-            dropdownInstances.push(dropdownInstance);
-
-            dropdownInstance.button.addEventListener('click', () =>{
-                dropdownInstance.toggle();
-            });
-        });
-
-        setupEventListeners(dropdownInstances);
-    });
-}
-
-initialize();
+console.log("This is goldenvertex's default template");
